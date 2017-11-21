8f0252aa7be9ea081b3efa026a3ad930f7947ef7
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-21 22:20:51 +0000

    added forms and made methods for create and update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,26 +2,63 @@ class PostsController < ApplicationController
 
   def index
 
-    @title = "This is the index page title!"
+    @posts = Post.all
 
   end
 
   def show
-
-    @id = params[:id]
-
+    @post = Post.find(params[:id])
   end
 
   def edit
-
-    @id = params[:id]
-
+    @post = Post.find(params[:id])
   end
 
   def new
 
-    @title = "This is the page where you would enter a new post"
+    @post = Post.new
 
+  end
+
+  def create
+
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def create
+
+    "This will show when you try to create a page using post from postman"
+
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
 end
