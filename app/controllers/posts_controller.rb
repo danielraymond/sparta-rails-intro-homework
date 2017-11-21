d9891a6162e6fdd427cd3f40d0054c7c97f9b54c
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-21 21:18:11 +0000

    added routes and controller methods for get verb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,22 @@ class PostsController < ApplicationController
 
   end
 
+  def show
+
+    @id = params[:id]
+
+  end
+
+  def edit
+
+    @id = params[:id]
+
+  end
+
+  def new
+
+    @title = "This is the page where you would enter a new post"
+
+  end
+
 end
